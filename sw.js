a0103fbe2f72d68d9986211331e0b9a1a0c6ed13
AlvinNiu <912806786@qq.com>
2018-09-13 15:43:09 +0800

    增加我的第一篇博客

--- sw.js
+++ sw.js
@@ -32,9 +32,8 @@ const PRECACHE_LIST = [
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
-  "yanshuo.io",
-  "cdnjs.cloudflare.com"
+  "alvin.onloading.cn",
+  "AlvinNiu.github.io"
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
 
